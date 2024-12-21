3f1ea62314988b286423a7fd428864b4474a6349
joecom7 <giovanni.francesco11c@gmail.com>
2024-12-21 15:48:52 +0100

    added tentative for day 21

--- 21/solution.py
+++ 21/solution.py
@@ -22,7 +22,9 @@ DIRECTIONAL_KEYPAD_MAP = {
     (1, 2): ">",
 }
 
-DIRECTIONAL_KEYPAD_MAP_INVERTED = dict((v, k) for k, v in DIRECTIONAL_KEYPAD_MAP.items())
+DIRECTIONAL_KEYPAD_MAP_INVERTED = dict(
+    (v, k) for k, v in DIRECTIONAL_KEYPAD_MAP.items()
+)
 
 DIR_MAP = {
     "^": (-1, 0),
@@ -31,7 +33,10 @@ DIR_MAP = {
     "v": (1, 0),
 }
 
+BACKTRACKING_MAP_REVERSED = dict((v, k) for k, v in DIR_MAP.items())
 N_MIDDLE_ROBOTS = 2
+N_MIDDLE_ROBOTS_PART_TWO = 10
+
 
 class OutOfKeypadException(Exception):
     pass
@@ -41,106 +46,171 @@ def add_tuple(a, b):
     return (a[0] + b[0], a[1] + b[1])
 
 
-def get_reached_config(move, current_config):
-    middle_robot_configs = current_config[0]
-    last_robot_config = current_config[1]
-    new_middle_robot_configs = []
-    pressed_last = False
+def sub_tuple(a, b):
+    return (a[0] - b[0], a[1] - b[1])
+
+
+def get_possible_configs(is_last_robot=True):
+    configs = []
+    for p1 in DIRECTIONAL_KEYPAD_MAP.keys():
+        if is_last_robot:
+            keyset = NUMERICAL_KEYPAD_MAP.keys()
+        else:
+            keyset = DIRECTIONAL_KEYPAD_MAP.keys()
+        for p2 in keyset:
+            configs.append((p1, p2))
+    return tuple(configs)
+
+
+def get_reached_config(move, robots_config, is_last_robot=True):
 
     current_move = move
-    for middle_robot_config in middle_robot_configs:
-        if current_move in DIR_MAP:
-            dir_to_move = DIR_MAP[current_move]
-            middle_robot_config = add_tuple(middle_robot_config, dir_to_move)
-            if middle_robot_config not in DIRECTIONAL_KEYPAD_MAP:
-                raise OutOfKeypadException()
-            current_move = "_"
-        elif current_move == "A":
-            current_move = DIRECTIONAL_KEYPAD_MAP[middle_robot_config]
-        new_middle_robot_configs.append(middle_robot_config)
+    middle_robot_config = robots_config[0]
+    robot_config = robots_config[1]
 
     if current_move in DIR_MAP:
         dir_to_move = DIR_MAP[current_move]
-        last_robot_config = add_tuple(last_robot_config, dir_to_move)
-        if last_robot_config not in NUMERICAL_KEYPAD_MAP:
+        middle_robot_config = add_tuple(middle_robot_config, dir_to_move)
+        if middle_robot_config not in DIRECTIONAL_KEYPAD_MAP:
             raise OutOfKeypadException()
-    #elif current_move == "A":
-    #    pressed_last = True
-    return tuple(new_middle_robot_configs), last_robot_config
+        current_move = "_"
+    elif current_move == "A":
+        current_move = DIRECTIONAL_KEYPAD_MAP[middle_robot_config]
+
+    if current_move in DIR_MAP:
+        dir_to_move = DIR_MAP[current_move]
+        robot_config = add_tuple(robot_config, dir_to_move)
+        if is_last_robot and robot_config not in NUMERICAL_KEYPAD_MAP:
+            raise OutOfKeypadException()
+        elif not is_last_robot and robot_config not in DIRECTIONAL_KEYPAD_MAP:
+            raise OutOfKeypadException()
+
+    return middle_robot_config, robot_config
 
 
-def get_possible_configs(n_middle_robots, depth=0):
-    if depth == n_middle_robots:
-        empty_tuple = ()
-        return tuple([(empty_tuple, button) for button in NUMERICAL_KEYPAD_MAP.keys()])
-    recursive_output = get_possible_configs(n_middle_robots, depth + 1)
-    possible_configs = []
-    for config in recursive_output:
-        for current_config in DIRECTIONAL_KEYPAD_MAP.keys():
-            new_config = [current_config]
-            new_config.extend(config[0])
-            possible_configs.append(tuple([tuple(new_config), config[1]]))
-    return tuple(possible_configs)
+def get_reaching_config(move, robots_config, is_last_robot=True):
 
-def get_base_config(n_middle_robots,last_robot_pos):
-    middle_robots_poses = []
-    for _ in range(0,n_middle_robots):
-        middle_robots_poses.append(DIRECTIONAL_KEYPAD_MAP_INVERTED['A'])
-    return tuple([tuple(middle_robots_poses),last_robot_pos])
+    current_move = move
+    middle_robot_config = robots_config[0]
+    robot_config = robots_config[1]
 
-def get_shortest_path_to_press(current_config, to_press):
+    if current_move in DIR_MAP:
+        dir_to_move = DIR_MAP[current_move]
+        middle_robot_config = sub_tuple(middle_robot_config, dir_to_move)
+        if middle_robot_config not in DIRECTIONAL_KEYPAD_MAP:
+            raise OutOfKeypadException()
+        current_move = "_"
+    elif current_move == "A":
+        current_move = DIRECTIONAL_KEYPAD_MAP[middle_robot_config]
 
+    if current_move in DIR_MAP:
+        dir_to_move = DIR_MAP[current_move]
+        robot_config = sub_tuple(robot_config, dir_to_move)
+        if is_last_robot and robot_config not in NUMERICAL_KEYPAD_MAP:
+            raise OutOfKeypadException()
+        elif not is_last_robot and robot_config not in DIRECTIONAL_KEYPAD_MAP:
+            raise OutOfKeypadException()
+
+    return middle_robot_config, robot_config
+
+
+def bfs_robot(pos, target_pos, is_last_robot=True, is_first_robot=False):
     queue = []
     costs = {}
 
-    middle_robot_configs = current_config[0]
-    last_robot_config = current_config[1]
-    n_middle_robots = len(middle_robot_configs)
-    to_reach = get_base_config(n_middle_robots,NUMERICAL_KEYPAD_MAP_INVERTED[to_press])
+    middle_robot_pos = DIRECTIONAL_KEYPAD_MAP_INVERTED["A"]
+    initial_config = (middle_robot_pos, pos)
+    target_config = (middle_robot_pos, target_pos)
 
-    # for j in range(0, n_rows):
-    #    for i in range(0, n_cols):
-    #        if board[(j, i)] != "#":
-    #            if (j, i) == start:
-    #                costs[(j, i)] = 0
-    #            else:
-    #                costs[(j, i)] = float("inf")
-
-    for config in get_possible_configs(n_middle_robots):
-        if config == current_config:
+    for config in get_possible_configs(is_last_robot):
+        if config == initial_config:
             costs[config] = 0
         else:
             costs[config] = float("inf")
 
-    queue.append(current_config)
+    queue.append(initial_config)
 
     while len(queue) != 0:
         config = queue.pop(0)
         smallest_distance = costs[config]
 
-        if config == to_reach:
+        if config == target_config:
             break
-                    
+
         for move in DIRECTIONAL_KEYPAD_MAP_INVERTED.keys():
             try:
-                reached_config = get_reached_config(move,config)
-                if costs[reached_config] == float('inf'):
+                reached_config = get_reached_config(move, config, is_last_robot)
+                if costs[reached_config] == float("inf"):
                     costs[reached_config] = smallest_distance + 1
                     queue.append(reached_config)
             except OutOfKeypadException:
                 pass
 
-    # print(costs)
+    # backtracking
 
-    return costs[to_reach] + 1
+    path, _ = backtrack(
+        target_config, initial_config, costs, is_last_robot, is_first_robot
+    )
 
-def get_shortest_path_for_code(code,n_middle_robots):
-    shortest_path = 0
-    current_config = (get_base_config(n_middle_robots,NUMERICAL_KEYPAD_MAP_INVERTED['A']))
+    return path + "A"
+
+
+def backtrack(config, start_config, costs, is_last_robot, is_first_robot):
+    if config == start_config:
+        return "", True
+    for move in DIRECTIONAL_KEYPAD_MAP_INVERTED.keys():
+        try:
+            reaching = get_reaching_config(move, config, is_last_robot)
+            if costs[reaching] == costs[config] - 1:
+                rv = backtrack(
+                    reaching, start_config, costs, is_last_robot, is_first_robot
+                )
+                if rv[1]:
+                    if not is_first_robot:
+                        if move == "A":
+                            return rv[0] + DIRECTIONAL_KEYPAD_MAP[config[0]], True
+                        else:
+                            return rv[0], True
+                    else:
+                        return rv[0] + move, True
+        except OutOfKeypadException:
+            pass
+    return "", False
+
+
+def get_shortest_path_for_code(code, n_middle_robots):
+    path = ""
+    previous_letter = "A"
     for letter in code:
-        shortest_path += get_shortest_path_to_press(current_config, letter)
-        current_config = get_base_config(n_middle_robots,NUMERICAL_KEYPAD_MAP_INVERTED[letter])
-    return shortest_path
+        path += get_shortest_path_for_letter_recursive(
+            previous_letter, letter, n_middle_robots
+        )
+        previous_letter = letter
+    return path
+
+
+def get_shortest_path_for_letter_recursive(
+    previous_letter, letter, n_middle_robots, depth=0
+):
+    if n_middle_robots == depth:
+        return letter
+    if depth == 0:
+        relevant_map = NUMERICAL_KEYPAD_MAP_INVERTED
+    else:
+        relevant_map = DIRECTIONAL_KEYPAD_MAP_INVERTED
+    last_robot_pos = relevant_map[previous_letter]
+    target_pos = relevant_map[letter]
+    path = ""
+    previous_letter = "A"
+    for letter in bfs_robot(
+        last_robot_pos, target_pos, (depth == 0), (depth + 1 == n_middle_robots)
+    ):
+        path += get_shortest_path_for_letter_recursive(
+            previous_letter, letter, n_middle_robots, depth + 1
+        )
+        previous_letter = letter
+    return path
+
 
 codes = []
 
@@ -149,15 +219,11 @@ with open("./21/input.txt", "r") as file:
         line = line.strip()
         codes.append(line)
 
-#print(codes)
+# print(codes)
 
 total_complexity = 0
 
 for code in codes:
-    shortes_path_for_code = get_shortest_path_for_code(code,N_MIDDLE_ROBOTS)
-    #print(f"shortest path for code {code}: {shortes_path_for_code}")
-    complexity = int(code[:-1]) * shortes_path_for_code
-    #print(f"complexity: {complexity}")
-    total_complexity += complexity
-    
-print(f"part 1 solution: {total_complexity}")
+    print(get_shortest_path_for_code(code, 2))
+
+# print(f"part 1 solution: {total_complexity}")
