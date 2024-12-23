b05e61fea28da0e1620c50762188a1b3eee1b4e8
joecom7 <giovanni.francesco11c@gmail.com>
2024-12-23 13:49:06 +0100

    changed solution to not use libraries

--- 13/solution.py
+++ 13/solution.py
@@ -1,61 +1,59 @@
-import math
 import re
-from sympy import symbols, Eq, solve
-from sympy.solvers.diophantine import diophantine
 
 pattern = r"Button\ A:\ X\+(\d+),\ Y\+(\d+)(\r\n|\r|\n)Button\ B:\ X\+(\d+),\ Y\+(\d+)(\r\n|\r|\n)Prize:\ X=(\d+),\ Y=(\d+)"
 
-def search_min_cost(a_value,b_value,prize_pos):
-    
-    x, y, t = symbols('x y t')
-    eq1 = a_value[0]*x + b_value[0]*y - prize_pos[0]
-    eq2 = a_value[1]*x + b_value[1]*y - prize_pos[1]
-    try:
-        sol = diophantine(eq1, t, syms=[x, y])
-        [xt, yt], = sol
-        eq3 = eq2.subs({x:xt, y:yt})
-        t1, = eq3.free_symbols
-        [t1s], = diophantine(eq3, y, syms=[t1])
-        rep = {t1:t1s}
-        return True,3*xt.subs(rep) + yt.subs(rep)
-    except:
-        return False,0
-    
+
+def search_min_cost(a_value, b_value, prize_pos):
+
+    det = a_value[0] * b_value[1] - b_value[0] * a_value[1]
+    xval = round((1 / det) * (prize_pos[0] * b_value[1] - b_value[0] * prize_pos[1]))
+    yval = round((1 / det) * (a_value[0] * prize_pos[1] - prize_pos[0] * a_value[1]))
+    if (
+        xval * a_value[0] + yval * b_value[0] == prize_pos[0]
+        and xval * a_value[1] + yval * b_value[1] == prize_pos[1]
+    ):
+        return True, 3 * xval + yval
+    else:
+        return False, 0
+
+
 total_price = 0
 
-with open('./13/input.txt', 'r') as file:
+with open("./13/input.txt", "r") as file:
     data = file.read()
-    
+
     # Find all matches in the string
     matches = re.findall(pattern, data)
 
     for match in matches:
-        
-        found,cost = search_min_cost(
-              (int(match[0]),int(match[1])),
-              (int(match[3]),int(match[4])),
-              (int(match[6]),int(match[7])))
+
+        found, cost = search_min_cost(
+            (int(match[0]), int(match[1])),
+            (int(match[3]), int(match[4])),
+            (int(match[6]), int(match[7])),
+        )
         if found:
             total_price += cost
-            
+
 print(f"part 1 solution: {total_price}")
 
 total_price = 0
 
-with open('./13/input.txt', 'r') as file:
+with open("./13/input.txt", "r") as file:
     data = file.read()
-    
+
     # Find all matches in the string
     matches = re.findall(pattern, data)
 
     for match in matches:
-        
-        found,cost = search_min_cost(
-              (int(match[0]),int(match[1])),
-              (int(match[3]),int(match[4])),
-              (10000000000000 + int(match[6]),10000000000000 + int(match[7])))
+
+        found, cost = search_min_cost(
+            (int(match[0]), int(match[1])),
+            (int(match[3]), int(match[4])),
+            (10000000000000 + int(match[6]), 10000000000000 + int(match[7])),
+        )
         if found:
             total_price += cost
             # print("found solution!")
-            
+
 print(f"part 2 solution: {total_price}")
--- 17/solution.py
+++ 17/solution.py
@@ -1,29 +1,28 @@
-
 class Computer:
     __ip = 0
-    __A  = 0
-    __B  = 0
-    __C  = 0
+    __A = 0
+    __B = 0
+    __C = 0
     __program = []
     __already_printed = False
     __output = ""
-    
-    def __init__(self,A_i,B_i,C_i,program):
+
+    def __init__(self, A_i, B_i, C_i, program):
         self.__A = A_i
         self.__B = B_i
         self.__C = C_i
         self.__program = program
-    
+
     def print(self):
         print(f"A content: {self.__A}")
         print(f"B content: {self.__B}")
         print(f"C content: {self.__C}")
         print(f"program: {self.__program}")
-        
+
     def execute_program(self):
         while self.__ip >= 0 and self.__ip < len(program):
             opcode = self.__program[self.__ip]
-            
+
             if opcode == 0:
                 self.adv()
             elif opcode == 1:
@@ -42,15 +41,19 @@ class Computer:
                 self.cdv()
             else:
                 raise "invalid opcode"
-            
-            print(f"next A = {self.__A}")
-            
+
+            #            print(f"next A = {self.__A}")
+
             self.__ip += 2
-            
-    def execute_program_until_different(self,correct_output):
-        while self.__ip >= 0 and self.__ip < len(program) and self.__output in correct_output:
+
+    def execute_program_until_different(self, correct_output):
+        while (
+            self.__ip >= 0
+            and self.__ip < len(program)
+            and self.__output in correct_output
+        ):
             opcode = self.__program[self.__ip]
-            
+
             if opcode == 0:
                 self.adv()
             elif opcode == 1:
@@ -69,10 +72,10 @@ class Computer:
                 self.cdv()
             else:
                 raise "invalid opcode"
-            
+
             self.__ip += 2
-        
-    def get_combo(self,val):
+
+    def get_combo(self, val):
         if val >= 0 and val <= 3:
             return val
         if val == 4:
@@ -83,8 +86,8 @@ class Computer:
             return self.__C
         else:
             raise "invalid combo operand"
-        
-    def find_A_value(self,A_start=0):
+
+    def find_A_value(self, A_start=0):
         A_current = A_start
         correct_output = ""
         for val in program:
@@ -93,96 +96,100 @@ class Computer:
             correct_output += str(val)
         while True:
             self.__ip = 0
-            self.__A  = A_current
-            self.__B  = 0
-            self.__C  = 0
+            self.__A = A_current
+            self.__B = 0
+            self.__C = 0
             self.__already_printed = False
             self.__output = ""
-            
+
             self.execute_program_until_different(correct_output)
-            
+
             if self.__output[:12] == correct_output[:12]:
-                #print(f"{self.__output}")
-                #print(f"  A = {A_current}")
+                # print(f"{self.__output}")
+                # print(f"  A = {A_current}")
                 print(f"  A = {int(bin(A_current).split('0b')[1])}")
-            
+
             if self.__output == correct_output:
                 break
-            
-            if(A_current%1000000 == 0):
+
+            if A_current % 1000000 == 0:
                 print(f"scanned until {A_current}")
-            
+
             A_current += 1
-        
+
         return A_current
-        
+
     def adv(self):
-        operand = self.get_combo(program[self.__ip+1])
+        operand = self.get_combo(program[self.__ip + 1])
         den = 2**operand
         self.__A = self.__A // den
-        
+
     def bxl(self):
-        operand = program[self.__ip+1]
+        operand = program[self.__ip + 1]
         self.__B = self.__B ^ operand
-        
+
     def bst(self):
-        operand = self.get_combo(program[self.__ip+1])
+        operand = self.get_combo(program[self.__ip + 1])
         self.__B = operand % 8
-        
+
     def jnz(self):
         if self.__A != 0:
-            operand = program[self.__ip+1]
+            operand = program[self.__ip + 1]
             self.__ip = operand - 2
-        
+
     def bxc(self):
         self.__B = self.__B ^ self.__C
-        
+
     def out(self):
         if self.__already_printed:
             self.__output += ","
         else:
             self.__already_printed = True
-        
-        operand = self.get_combo(program[self.__ip+1])
+
+        operand = self.get_combo(program[self.__ip + 1])
         operand = operand % 8
         self.__output += str(operand)
-        
+
     def bdv(self):
-        operand = self.get_combo(program[self.__ip+1])
+        operand = self.get_combo(program[self.__ip + 1])
         den = 2**operand
         self.__B = self.__A // den
-        
+
     def cdv(self):
-        operand = self.get_combo(program[self.__ip+1])
+        operand = self.get_combo(program[self.__ip + 1])
         den = 2**operand
         self.__C = self.__A // den
-        
+
     def print_output(self):
         print(self.__output)
+
 
 A_i = 0
 B_i = 0
 C_i = 0
 program = []
 
-with open('./17/input.txt', 'r') as file:
+with open("./17/input.txt", "r") as file:
     for line in file:
-        if(line.startswith("Register A: ")):
-            A_i = int(line[len("Register A: "):])
-        elif(line.startswith("Register B: ")):
-            B_i = int(line[len("Register B: "):])
-        elif(line.startswith("Register C: ")):
-            C_i = int(line[len("Register C: "):])
-        elif(line.startswith("Program: ")):
-            program = [num for num in [int(numstr) for numstr in line[len("Program: "):].split(",")]]
+        if line.startswith("Register A: "):
+            A_i = int(line[len("Register A: ") :])
+        elif line.startswith("Register B: "):
+            B_i = int(line[len("Register B: ") :])
+        elif line.startswith("Register C: "):
+            C_i = int(line[len("Register C: ") :])
+        elif line.startswith("Program: "):
+            program = [
+                num
+                for num in [
+                    int(numstr) for numstr in line[len("Program: ") :].split(",")
+                ]
+            ]
 
-A_i = 202366627359274
-computer = Computer(A_i,B_i,C_i,program)
+# A_i = 202366627359274
+computer = Computer(A_i, B_i, C_i, program)
 
 computer.execute_program()
-print("part 1 solution: ",end="")
+print("part 1 solution: ", end="")
 computer.print_output()
 
-#print(f"part 2 solution: {computer.find_A_value()}")
-
-
+# print(f"part 2 solution: {computer.find_A_value()}")
